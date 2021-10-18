d2b46e4eb46e2a7a2cd86089aff58d5b130f161c
MikhalyovDmitry <92030218+MikhalyovDmitry@users.noreply.github.com>
2021-10-18 23:37:22 +0300

    final

--- hw_1_base_operations/run.bat
+++ hw_1_base_operations/run.bat
@@ -1,3 +1,4 @@
+chcp 1251
 call mvn clean install
 call java -jar .\target\hw_1_base_operations.jar
 @pause
--- hw_2_strings/run.bat
+++ hw_2_strings/run.bat
@@ -1,3 +1,4 @@
+chcp 1251
 call mvn clean install
 call java -jar .\target\hw_2_strings.jar
 @pause
