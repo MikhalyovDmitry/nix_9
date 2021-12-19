5cf6ebe0fb8bb577141a1a731884f5cc7cab096d
MikhalyovDmitry <92030218+MikhalyovDmitry@users.noreply.github.com>
2021-12-19 23:27:25 +0200

    module_3 logging

---
+++ module_3/schema.sql
@@ -0,0 +1,7 @@
+insert into my_schema.orders values (1, 'Test User 001', 'qwerty', '0660948484');
+insert into my_schema.orders values (2, 'Test User 002', '012345', '0996557477');
+insert into my_schema.orders values (3, 'Test User 003', 'qqq22q', '0993285752');
+insert into my_schema.orders values (4, 'Test User 004', 'qasasa', '0664674636');
+insert into my_schema.orders values (5, 'Test User 005', '000000', '0996454354');
+insert into my_schema.orders values (6, 'Test User 006', 'zxcvbb', '0938473987');
+insert into my_schema.orders values (7, 'Test User 007', 'qwerty', '0678349348');
